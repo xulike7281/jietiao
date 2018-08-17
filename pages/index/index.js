e6fbbecafb8e393f816ce1962a2d01f45700711f
xuwei <xw49546@ly.com>
2018-08-17 17:16:50 +0800

    t

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,12 +9,7 @@ Page({
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
+
   onLoad: function () {
     if (app.globalData.userInfo) {
       this.setData({
@@ -46,9 +41,13 @@ Page({
   getUserInfo: function(e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
+    
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
+    })
+    wx.navigateTo({
+      url: '/pages/registerForm/registerForm'
     })
   }
 })
